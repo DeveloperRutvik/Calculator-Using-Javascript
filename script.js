0ce1352640946924f68c3cbca6a9a0765d5384aa
Rutvik <159632832+DeveloperRutvik@users.noreply.github.com>
2026-01-16 19:57:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    display.value = eval(display.value);
+}
+
+function clearSingleDigit() {
+    display.value = display.value.toString().slice(0, -1);
+}
